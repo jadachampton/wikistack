95139331699c9e7cd057013d1c9f6eecce3bda98
Anne Zhou <anne.zhou920@gmail.com>
2017-01-20 17:48:26 -0500

    setting up

---
+++ app.js
@@ -0,0 +1,5 @@
+var env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
